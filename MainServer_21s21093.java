e9077f8c6f4cd0f7774e2f3d50499cfff1530ff0
EklasAlsiyabi <135134405+EklasAlsiyabi@users.noreply.github.com>
2023-05-31 17:28:04 +0400

    Create MainServer_21s21093.java

---
+++ MainServer_21s21093.java
@@ -0,0 +1,8 @@
+public class MainServer_21s21093{
+    public static void main (String args[])
+    {
+        Server_21s21093 a_21S21093= new Server_21s21093();
+        Thread b_21S21093 = new Thread(a_21S21093);
+        b_21S21093.start();
+    } 
+}
