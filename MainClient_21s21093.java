4c0929a8071b2efa17fe10501b7bcb6351b5ec1f
EklasAlsiyabi <135134405+EklasAlsiyabi@users.noreply.github.com>
2023-05-31 17:26:40 +0400

    Create MainClient_21s21093.java

---
+++ MainClient_21s21093.java
@@ -0,0 +1,10 @@
+public class MainClient_21s21093 {
+     public static void main (String args[]){
+      
+
+       Client_21s21093 eklas_21s21093 = new Client_21s21093();
+        Thread SH_21S21093 = new Thread(eklas_21s21093);
+        SH_21S21093.start();
+    } 
+}
+
